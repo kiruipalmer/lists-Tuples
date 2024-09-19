e93c49056e793938eb72ed18be430eedf6984339
Dennis Kirui <dennispalmerson@gmail.com>
2024-09-19 12:53:20 +0300

    commit with all steps needed, 1st commit

---
+++ list.py
@@ -0,0 +1,33 @@
+#empty list
+my_list = []
+
+#appended list
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+#inserted
+my_list.insert(1, 15)
+print(my_list)
+
+#extend
+my_list.extend([50, 60, 70])
+print(my_list)
+
+#removed
+my_list.pop()
+print(my_list)
+
+#sort
+my_list.sort()
+print(my_list)
+
+#value finding
+index_of_30 = my_list.index(30)
+print(index_of_30)
+
+
+
